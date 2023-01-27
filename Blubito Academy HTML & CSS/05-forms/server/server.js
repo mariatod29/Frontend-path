5f9dc1180a86981167ccd57d928dcbfcd4590ba0
mariatod29 <m.todorova@blubito.com>
2023-01-27 11:06:04 +0200

    04-css-position, 05-forms, 06-flex, 07-grid

---
+++ Blubito Academy HTML & CSS/05-forms/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/', async (req, res, next) => {
+    console.log('OUR FORM BODY', req.body);
+    next();
+})
+app.get('/', async (req, res, next) => {
+    console.log('WORKS');
+    next();
+})
+
+
+app.listen(3001, () => {
+    console.log("Started on PORT 3001");
+})
